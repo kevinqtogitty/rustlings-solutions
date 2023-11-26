3b8b8f09ed3a500cfd7ca913a255ddd166ddcf03
Kevin To <kevin.to@gmail.com>
2023-11-25 20:39:39 -0500

    Completes all primitive types exercises

--- primitive_types/primitive_types3.rs
+++ primitive_types/primitive_types3.rs
@@ -5,10 +5,16 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    let a = 0..100;
+    //Or let a = ["Hello"; 100];
+
+    // 1..2;   // std::ops::Range
+    // 3..;    // std::ops::RangeFrom
+    // ..4;    // std::ops::RangeTo
+    // ..;     // std::ops::RangeFull
+    // 5..=6;  // std::ops::RangeInclusive
+    // ..=7;   // std::ops::RangeToInclusive
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
--- primitive_types/primitive_types4.rs
+++ primitive_types/primitive_types4.rs
@@ -5,13 +5,15 @@
 // Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    // Use the reference of a, and construct a slice with indexes 1-3 non-inclusive of the final element
+    // let nice_slice = &a[1..4];
+
+    // Or construct a a new array by accesing the values of a directly
+    let nice_slice = [a[1], a[2], a[3]];
 
     assert_eq!([2, 3, 4], nice_slice)
 }
