fb38c87f6dad17a9bc0b75b71b3bedd2aa51bda4
Kevin To <kevin.to@gmail.com>
2023-11-28 09:11:40 -0500

    Completes all module exercises

--- modules/modules2.rs
+++ modules/modules2.rs
@@ -7,12 +7,12 @@
 // Execute `rustlings hint modules2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 mod delicious_snacks {
     // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    // We're trying to re-export internal modules and renaming them as fruit and veggie,
+    // but only containing one of each public constant inside mod fruits and mod veggies
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";
