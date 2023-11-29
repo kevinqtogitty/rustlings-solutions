e7564c9fd80f2563419a29c5b0f98d64f0c23214
Kevin To <kevin.to@gmail.com>
2023-11-29 09:50:48 -0500

    Completes all hashmap exercises

--- hashmaps/hashmaps3.rs
+++ hashmaps/hashmaps3.rs
@@ -14,14 +14,13 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
+    name: String,
 }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
@@ -39,6 +38,33 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        // First check if the entry exists in the hashmap with .entry
+        // if it does, update scores with .and_modify
+        // if it doesn't, insert the team into the hashmap with .or_insert
+        scores
+            .entry(team_1_name.clone())
+            .and_modify(|team| {
+                team.goals_scored += team_1_score;
+                team.goals_conceded += team_2_score;
+            })
+            .or_insert(Team {
+                name: team_1_name,
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score
+            });
+
+        scores
+            .entry(team_2_name.clone())
+            .and_modify(|team| {
+                team.goals_scored += team_2_score;
+                team.goals_conceded += team_1_score;
+            })
+            .or_insert(Team {
+                name: team_2_name,
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score
+            });
     }
     scores
 }
