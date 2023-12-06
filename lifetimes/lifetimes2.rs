49428034d258d58c85b5f5c58c946f411c43bc68
Kevin To <kevin.to@gmail.com>
2023-12-06 10:12:34 -0500

    Completes all lifetime exercises

--- lifetimes/lifetimes2.rs
+++ lifetimes/lifetimes2.rs
@@ -6,8 +6,6 @@
 // Execute `rustlings hint lifetimes2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
@@ -19,9 +17,15 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 fn main() {
     let string1 = String::from("long string is long");
     let result;
+    // Bring string2 into the same scope of string1, so they both have the same lifetime which is needed to log out result
+    let string2 = String::from("xyz");
+    
     {
-        let string2 = String::from("xyz");
         result = longest(string1.as_str(), string2.as_str());
+        // Or we can bring println into the same scope of the computation
+        // println!("The longest string is '{}'", result);
+
+        // Or we can bring everything also into the same scope, inside the curlies
     }
     println!("The longest string is '{}'", result);
 }
